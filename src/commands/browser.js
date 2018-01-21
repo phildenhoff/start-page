ce0574d10d8d235adac5fabff02de9f38af5ae1b
Phil Denhoff <phildenhoff@gmail.com>
2018-01-20 22:58:58 -0800

    Refactored to eslint standard. Added default_function for Search

--- src/commands/browser.js
+++ src/commands/browser.js
@@ -1,8 +1,8 @@
 export let pd = {
-    "type":"browser",
-    "command":"pd",
-    "functionName":"pomodoro",
-    "title":"Pomodoro",
-    "helpCommand":"pd;[start (number of timers)] [reset] [pause] [resume]",
-    "helpDesc":"Start, reset, or pause a Pomodoro timer", // optional, defaults to "Search [TITLE]",
-}
+    'type':'browser',
+    'command':'pd',
+    'functionName':'pomodoro',
+    'title':'Pomodoro',
+    'helpCommand':'pd;[start (number of timers)] [reset] [pause] [resume]',
+    'helpDesc':'Start, reset, or pause a Pomodoro timer', // optional, defaults to 'Search [TITLE]'
+};
--- src/commands/search.js
+++ src/commands/search.js
@@ -1,98 +1,109 @@
-export let g = {
-  "type":"search",
-  "command":"g",
-  "url":"https://www.google.ca",
-  "search":"/search?q=",
-  "title":"Google",
-  "helpCommand":"g;[query]", // optional, defaults to "[COMMAND];[query]"
-  "helpDesc":"Search Google", // optional, defaults to "Search [TITLE]"
+export const default_function = function(command_info, user_cmd, tokens) {
+    let url;
+    // check that tokens is not empty. If it is, only go to base url
+    if (tokens.length > 0) {
+        url = command_info.baseUrl + command_info.search + tokens;
+    } else {
+        url = command_info.baseUrl;
+    }
+    window.open(url, '_blank');
+};
+
+export const g = {
+    'type':'search',
+    'command':'g',
+    'baseUrl':'https://www.google.ca',
+    'search':'/search?q=',
+    'title':'Google',
+    'helpCommand':'g;[query]', // optional, defaults to '[COMMAND];[query]'
+    'helpDesc':'Search Google', // optional, defaults to 'Search [TITLE]'
 };
 
 export let yt = {
-  "type":"search",
-  "command":"yt",
-  "url":"https://www.youtube.com",
-  "search":"/results?search_query=",
-  "title":"YouTube",
-}
+    'type':'search',
+    'command':'yt',
+    'baseUrl':'https://www.youtube.com',
+    'search':'/results?search_query=',
+    'title':'YouTube',
+};
 
 export let plex = {
-  "type":"search",
-  "command":"plex",
-  "url":"https://app.plex.tv/web/app#",
-  "search":"!/server/308101523183f80723efc9ce7e5e37c4371943a1/search/",
-  "title":"Plex",
-}
+    'type':'search',
+    'command':'plex',
+    'baseUrl':'https://app.plex.tv/web/app#',
+    'search':'!/server/308101523183f80723efc9ce7e5e37c4371943a1/search/',
+    'title':'Plex',
+};
 
 export let r = {
-  "type":"search",
-  "command":"r",
-  "url":"https://www.reddit.com",
-  "search":"/r/",
-  "title":"Reddit",
-  "helpCommand":"r;[subreddit]",
-  "helpDesc":"Go to subreddit",
-}
+    'type':'search',
+    'command':'r',
+    'baseUrl':'https://www.reddit.com',
+    'search':'/r/',
+    'title':'Reddit',
+    'helpCommand':'r;[subreddit]',
+    'helpDesc':'Go to subreddit',
+};
 
 export let dr = {
-  "type": "search",
-  "command": "dr",
-  "url": "https://drive.google.com/",
-  "search": "drive/u/0/search?q=",
-  "title": "Google Drive"
-}
+    'type': 'search',
+    'command': 'dr',
+    'baseUrl': 'https://drive.google.com/',
+    'search': 'drive/u/0/search?q=',
+    'title': 'Google Drive'
+};
 
 export let wa= {
-  "type":"search",
-  "command":"wa",
-  "url":"https://www.wolframalpha.com",
-  "search":"/input/?i=",
-  "title":"Wolfram|Alpha"
-}
+    'type':'search',
+    'command':'wa',
+    'baseUrl':'https://www.wolframalpha.com',
+    'search':'/input/?i=',
+    'title':'Wolfram|Alpha'
+};
 
 export let i = {
-  "type":"search",
-  "command":"i",
-  "url":"https://inbox.google.com",
-  "search":"/search/",
-  "title":"Google Inbox",
-  "helpDesc":"Search your Google Inbox",
-}
+    'type':'search',
+    'command':'i',
+    'baseUrl':'https://inbox.google.com',
+    'search':'/search/',
+    'title':'Google Inbox',
+    'helpDesc':'Search your Google Inbox',
+};
 
-export let h = {
-  "type":"search",
-  "command":"h",
-  "url":"https://hangouts.google.com",
-  "search":"",
-  "title":"Hangouts",
-  "helpCommand":"h;", // optional, defaults to "[COMMAND];[query]"
-  "helpDesc":"Open Hangouts", // optional, defaults to "Search [TITLE]"
-}
+export let ha = {
+    'type':'search',
+    'command':'ha',
+    'baseUrl':'https://hangouts.google.com',
+    'search':'',
+    'title':'Hangouts',
+    'helpCommand':'ha;', // optional, defaults to '[COMMAND];[query]'
+    'helpDesc':'Open Hangouts', // optional, defaults to 'Search [TITLE]'
+};
 
 export let am = {
-  "type":'search',
-  "command":"h",
-  "url":"https://www.amazon.ca",
-  "search":"/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=",
-  "title":"Amazon",
-  "helpCommand":"am;[product]",
-  "helpDesc":"Search Amazon"
-}
+    'type':'search',
+    'command':'h',
+    'baseUrl':'https://www.amazon.ca',
+    'search':'/s/ref=nb_sb_noss_2?baseUrl=search-alias%3Daps&field-keywords=',
+    'title':'Amazon',
+    'helpCommand':'am;[product]',
+    'helpDesc':'Search Amazon'
+};
 
 export let fcc = {
-  "type":"search",
-  "command":"fcc",
-  "url":"https://www.freecodecamp.com/",
-  "search":"",
-  "title":"Free Code Camp",
-  "helpCommand":"fcc;",
-  "helpDesc":"Open Free Code Camp"
-}
+    'type':'search',
+    'command':'fcc',
+    'baseUrl':'https://www.freecodecamp.com/',
+    'search':'',
+    'title':'Free Code Camp',
+    'helpCommand':'fcc;',
+    'helpDesc':'Open Free Code Camp'
+};
 
 export let u = {
-  "type":"search",
-  "command":"u",
-  "url":"https://",
-  "search":"",
-  "title":"URL",
-}
+    'type':'search',
+    'command':'u',
+    'baseUrl':'https://',
+    'search':'',
+    'title':'URL',
+};
